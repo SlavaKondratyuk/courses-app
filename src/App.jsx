91d0a9da6558b74e7a160cedfbaa3d2bb5771bb7
Stas <jobslava4ua@gmail.com>
2023-03-13 17:44:01 +0200

    linter/prettier set up done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './components/Header/Header';
+
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<div>React</div>
+			<Header />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Logo from './components/Logo/Logo';
+import Button from '../common/Button/Button';
+
+import './Header.css';
+
+function LogData() {
+	console.log('Log out');
+}
+
+function Header() {
+	const name = 'Dave';
+
+	return (
+		<div className='header'>
+			<div className='logo-wrapper'>
+				<Logo />
+			</div>
+			<div>
+				<p>{name}</p>
+				<Button name='Log out' clickHandler={LogData} />
+			</div>
+			<p className='p-class'></p>
+		</div>
+	);
+}
+
+export default Header;
